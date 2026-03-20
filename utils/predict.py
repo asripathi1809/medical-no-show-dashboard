7e1b11d994c61975778d4f8490ddd9aeaf6deb8e
Ananya Sripathi <ananya.sripathi@gmail.com>
2026-03-20 17:12:35 -0400

    Update predict.py

--- utils/predict.py
+++ utils/predict.py
@@ -7,19 +7,25 @@ MODEL_URL = "https://drive.google.com/uc?id=1P1Vr53XafaGaZVqxnXCqNvZRiZo_qrGU"
 
 @st.cache_resource
 def load_model():
-    """Download and cache the model."""
-    if not os.path.exists("model.pkl"):
+    """Download and cache the trained pipeline"""
+    if not os.path.exists("mo_pipeline.pkl"):
         response = requests.get(MODEL_URL)
-        with open("model.pkl", "wb") as f:
+        with open("mo_pipeline.pkl", "wb") as f:
             f.write(response.content)
-    return joblib.load("model.pkl")
+    return joblib.load("mo_pipeline.pkl")
 
 def make_prediction(model, data):
-    """
-    Make prediction.
-    Assumes model.predict_proba is available (classification model).
-    Returns probability of 'No-show'.
-    """
-    # Keep as DataFrame for safety
+    """Ensure features match the model and return probability of NoShow"""
+    expected_features = model.feature_names_in_
+
+    # Add missing columns
+    for col in expected_features:
+        if col not in data.columns:
+            data[col] = 0
+
+    # Reorder columns
+    data = data[expected_features]
+
+    # Predict probability
     prob = model.predict_proba(data)[:,1][0]
     return prob
